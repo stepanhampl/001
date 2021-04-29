52cdac79917d88056516fed136ac6e9196f29479
stepanhampl <koblihh1@gmail.com>
2021-04-29 09:57:58 +0200

    draft

---
+++ scripts/script002-index.js
@@ -0,0 +1,6 @@
+// body = document.getElementsByTagName("body")
+// showTime = document.getElementById("showTime")
+
+// window.setInterval(function () { showTime.innerHTML = new Date()}, 1000);
+
+// main.style.backgroundColor = red
